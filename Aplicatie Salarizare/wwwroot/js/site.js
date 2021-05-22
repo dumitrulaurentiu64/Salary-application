9b23a0d9c3530167d9f5ce9b39cdcda6f77a1975
dumitrulaurentiu64 <51172884+dumitrulaurentiu64@users.noreply.github.com>
2021-05-22 21:50:52 +0300

    Delete Aplicatie Salarizare directory

--- Aplicatie Salarizare/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿$(function () {
-    $('#main-content').on('click', '.container a', function () {
-        var url = $(this).attr('href');
-        $('#main-content-data').load(url);
-
-        return false;
-    })
-});
